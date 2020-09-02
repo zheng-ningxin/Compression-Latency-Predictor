2f2e88088a62bbba840bab2b870289a457a27019
Ningxin <Ningxin.Zheng@microsoft.com>
2020-09-02 13:22:16 +0000

    add the config file for vgg11

--- example.py
+++ example.py
@@ -1,11 +1,17 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 import torch
+import argparse
 import torchvision
 from lib.latency_predictor import LatencyPredictor
-# net = torchvision.models.resnet101().cuda()
-net = torchvision.models.mobilenet_v2().cuda()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', help='model to measure the latency')
+parser.add_argument('--config', help='the config file for the latency measurement')
+args = parser.parse_args()
+Model = getattr(torchvision.models, args.model)
+net = Model().cuda()
 
 lp = LatencyPredictor(net, torch.zeros(16, 3, 224, 224).cuda())
 # lp.build('./config/example.yaml')
-lp.generate_dataset('./config/mobilenet.yaml')
+lp.generate_dataset(args.config)
